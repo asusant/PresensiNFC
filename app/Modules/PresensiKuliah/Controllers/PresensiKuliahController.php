4dde0b59216f8e5237b72afb97fe2b3a586af2e9
Hendi Susanto <hendi.susanto@students.unnes.ac.id>
2019-03-05 08:19:13 +0700

    tgl

--- app/Modules/PresensiKuliah/Controllers/PresensiKuliahController.php
+++ app/Modules/PresensiKuliah/Controllers/PresensiKuliahController.php
@@ -96,6 +96,7 @@ class PresensiKuliahController extends Controller
 							'pertemuan_kuliah.*',
 							'mata_kuliah.id as id_mata_kuliah',
 							'users.name as created_by',
+							'users.name as created_at',
 						]);
 
 		$datatables = Datatables::of($data);
